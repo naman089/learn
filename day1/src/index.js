eeddd4df1f6cf534bcb152a7a1cef967f709cd2b
naman089 <109947870+naman089@users.noreply.github.com>
2023-07-28 16:03:43 +0530

    Add files via upload

---
+++ day1/src/index.js
@@ -0,0 +1,9 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<React.Fragment><h1>Hello world!</h1>
+<p>hello this is paragraph tag in jsx</p>
+<h2>hello this is h2 tag </h2>
+<h3>this is h3 tag</h3></React.Fragment> ,
+ document.getElementById("root"));
